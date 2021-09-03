30eafe792788d33c70926d0410fc5647a7b71c86
David Tomschitz <david@tomschitz.de>
2021-09-03 18:46:57 +0200

    feat: implement the layer filter functions

--- mapbox_gl_platform_interface/lib/src/method_channel_mapbox_gl.dart
+++ mapbox_gl_platform_interface/lib/src/method_channel_mapbox_gl.dart
@@ -694,6 +694,12 @@ class MethodChannelMapboxGl extends MapboxGlPlatform {
   }
 
   @override
+  Future<void> setFilter(String layerId, dynamic filter) async {
+    await _channel.invokeMethod('style#setFilter',
+        <String, dynamic>{'layerId': layerId, 'filter': filter});
+  }
+
+  @override
   Future<LatLng> toLatLng(Point screenLocation) async {
     try {
       var latLngMap =
